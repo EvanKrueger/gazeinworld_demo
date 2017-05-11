83bd78cb0eba47aff5d57a3ae70fc6f9a2e66d08
Evan Krueger <krueger_dev@icloud.com>
2017-05-11 14:35:48 -0400

    Creating develop branch, backup before dist upgrade.

--- main.py
+++ main.py
@@ -3,16 +3,14 @@ This module is a stand-in for the ipython notebook that will be used in the demo
 """
 from __future__ import division, print_function
 from Modules.PerformParser import dictFileToDataFrame
+from Modules.timeManager import TimeMgr
 from Modules.classes import *
 
-# define nan var for dataframe
+# define nan var for "NaN"-encoded-floats in dataframe
 nan = float("NaN")
 # read dict file, save as pandas DF
 datafile = dictFileToDataFrame(
     "Data/2017-4-14-18-36/exp_data-2017-4-14-18-36.dict")
 
-# create shape node
-head = Head(datafile, "viewMat_4x4")
-eye_L = Eye(datafile, "leftEyeMat_4x4", head, "leftEyeInHead_XYZ")
-
-print(eye_L.gazeinworld())
+# create time manager instance for controlling global time
+time = TimeMgr(datafile)
